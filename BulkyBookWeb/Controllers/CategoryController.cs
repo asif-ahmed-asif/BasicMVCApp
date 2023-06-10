fc0efd12433f8c3a06b8258fbf64871f39a46031
Anonymous <asifahmed1132@gmail.com>
2023-06-10 21:46:22 +0600

    custom css added, design changed and fetch categories from DB

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            //var categories = _db.Categories.ToList();
+
+            //strongly typed
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
         }
     }
 }
